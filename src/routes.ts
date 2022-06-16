23108f2dde082b658e4d19ae2baed16a4f82a619
Victor <victorabiscula@live.com>
2022-06-16 18:32:08 -0300

    criado novas rotas do tipo get

--- src/routes.ts
+++ src/routes.ts
@@ -4,3 +4,5 @@ import { Person } from './Controller/PersonControl'
 export const routes = express.Router()
 
 routes.post('/person', Person.personData)
+routes.get('/people', Person.getAllPeople)
+routes.get('/person/:cpf', Person.getPerson)
