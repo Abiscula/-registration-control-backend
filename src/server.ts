ddc7ca40d418b1de0e0fbcc4fca69b893ef4522c
Victor <victorabiscula@live.com>
2022-06-16 09:16:59 -0300

    adicionado chamada para o db

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { routes } from './routes'
+import { startDB } from './database/config'
 
 export const app = express()
 const port = 3333
@@ -8,6 +9,8 @@ const port = 3333
 app.use(express.json())
 app.use(cors())
 app.use(routes)
+
+startDB()
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
