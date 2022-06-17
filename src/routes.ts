a3a6a8ac057c2a0e0ee3079dd35a4180cd58b96a
Victor <victorabiscula@live.com>
2022-06-17 08:48:36 -0300

    criado rota post e get para contratos

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { Contract } from './Controller/ContractControl'
 import { Person } from './Controller/PersonControl'
 
 export const routes = express.Router()
@@ -6,3 +7,6 @@ export const routes = express.Router()
 routes.post('/person', Person.personData)
 routes.get('/people', Person.getAllPeople)
 routes.get('/person/:cpf', Person.getPerson)
+
+routes.post('/contract', Contract.contractData)
+routes.get('/contracts', Contract.getAllContracts)
